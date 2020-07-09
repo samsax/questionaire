ed59eb3285ba5537a7e0e5cff0dcb7eac34d6788
samsax <samuelsaxofon@gmail.com>
2020-07-09 12:50:33 -0500

    conexion base de datos

--- DjangoCovid/settings.py
+++ DjangoCovid/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'DjangoCovid.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'tuyrmzrw',
+            'USER': 'tuyrmzrw',
+            'PASSWORD': 'E4BXxIYSAaO89rAI5NGfJjKzS9tFy_V3',
+            'HOST': 'ruby.db.elephantsql.com',
+            'PORT': '5432',
     }
 }
 
