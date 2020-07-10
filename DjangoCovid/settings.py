9aef4fe9a1daefcbb9e6d44981920d64bde6ea21
samsax <samuelsaxofon@gmail.com>
2020-07-09 21:08:43 -0500

    Configuraciones heroku

--- DjangoCovid/settings.py
+++ DjangoCovid/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import os
 import dj_database_url
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,4 +137,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+django_heroku.settings(locals())
